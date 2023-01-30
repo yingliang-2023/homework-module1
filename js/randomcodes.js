8d3b29c4bdbcc0692abdc4bf63a36c5d9cefe070
YING LIANG <y.liang.168@westcliff.edu>
2023-01-29 20:04:48 -0600

    replace partner js

---
+++ js/randomcodes.js
@@ -0,0 +1,7 @@
+//function to generate combination of characters
+
+function generateCode(){
+    var code='';
+    var str='ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+    
+}
