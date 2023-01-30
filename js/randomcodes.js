171329869deacc4d40c7d1589622fbf27f48deec
YING LIANG <y.liang.168@westcliff.edu>
2023-01-29 20:29:32 -0600

    randomcodes added

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -3,5 +3,58 @@
 function generateCode(){
     var code='';
     var str='ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-    
+
+    //generate character multiple times using a loop
+    for (var i=1; i<=8;i++){
+        var char=Math.random()*str.length;
+        code +=str.charAt(char);
+    }
+    return code;
 }
+
+
+document.getElementById('codes').innerHTML=generateCode();
+
+function disableButton(){
+    document.getElementById("submit").disabled=true;
+
+}
+
+disableButton();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
