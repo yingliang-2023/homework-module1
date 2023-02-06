84bada89ee75f3d9c5b7176fbf5b2dc4105b0522
YING LIANG <y.liang.168@westcliff.edu>
2023-02-06 01:47:16 -0600

    box added

--- js/activity.js
+++ js/activity.js
@@ -3,15 +3,32 @@
 $(document).ready(function(){
 
 
-$(".selectable").click(clickedCell);
+$("td").click(function(){
+    var content=$(this).text();
+    console.log(content);
+    if (content != "Not Available"){
+        $(this).toggleClass("tdhighlight");
+    }
 
-function clickedCell(){
-    var selectedText=$(this).attr("id");
-    
-    var selectedId="#"+selectedText;
-   
-    $(selectedId).toggleClass("clickedCell");
-};
+if($(this).hasClass('tdhighlight')){
+   console.log($(this).attr("class"));
+    $("#displaySelected").css("visibility","visible");
+    $("#displaySelected").css("margin-top","2em");
+    $('#result').append("<p>"+content+"</p>");
+}else{
+    $('#result p:contains('+content+')').remove();
+    if($('#result').has('p').length==false){
+        $('#displaySelected').css("visibility","hidden");
+        $('#displaySelected').css("margin-top","0");}
+
+    };
+
+});
 
 
 });
+
+
+
+
+
