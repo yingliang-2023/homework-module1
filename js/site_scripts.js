d3b4511308336a9058c41d7b0fdad835dc2492c6
YING LIANG <y.liang.168@westcliff.edu>
2023-02-20 00:04:22 -0600

    wk3 commit

--- js/site_scripts.js
+++ js/site_scripts.js
@@ -1,21 +1,21 @@
-var photo=[];
-        var fileNames=[];
-        var imageList=[];
-        var image;
-        var openList="<li class='partner'>";
-        var closeList="</li>";
+// var photo=[];
+//         var fileNames=[];
+//         var imageList=[];
+//         var image;
+//         var openList="<li class='partner'>";
+//         var closeList="</li>";
 
-        for (var i=0; i<6; i++){
-            fileNames.push("partner"+(i+1));
-            photo.push("<img src='images/partners/"+fileNames[i]+".png'>");
-            image=openList+photo[i]+closeList;
-            imageList.push(image);
-        }
+//         for (var i=0; i<6; i++){
+//             fileNames.push("partner"+(i+1));
+//             photo.push("<img src='images/partners/"+fileNames[i]+".png'>");
+//             image=openList+photo[i]+closeList;
+//             imageList.push(image);
+//         }
 
-        //insert all final image elements in the album.
+//         //insert all final image elements in the album.
 
-        //Display
-        document.getElementById("partners").innerHTML=imageList.join(" ");
+//         //Display
+//         document.getElementById("partners").innerHTML=imageList.join(" ");
 
 
         
